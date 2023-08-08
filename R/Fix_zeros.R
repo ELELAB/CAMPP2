f77a6da2a819a700adf895e6490f4b72a4ef6b75
nikolatom <1nikola.tom@gmail.com>
2023-08-08 14:03:15 +0200

    SE integration in progress

--- R/Fix_zeros.R
+++ R/Fix_zeros.R
@@ -12,9 +12,10 @@
 #' represented by a column from a metadata file).
 #' @param remove.sparse.features a logical argument (TRUE/FALSE) for removal of
 #' features with sum of zero counts larger than the size of the smallest
-#' sample group, and for replacing the remaining zeros.  Default is TRUE.
+#' sample group, and for replacing the remaining zeros. Default is TRUE.
 #' @export
 #' @import impute
+#' @import SummarizedExperiment
 #' @return a data frame with fixed zeros. Features having sum of zero counts
 #' higher than the size of the smallest sample group are removed and
 #' remaining zeros will be replaced by default.
@@ -24,14 +25,15 @@
 #' }
 
 
-FixZeros <- function(data, group, remove.sparse.features=TRUE) {
-    if(any(data==0)){
+FixZeros <- function(SE, group, remove.sparse.features=TRUE) {
+
+    if(any(assay(SE)==0)){
         print("data includes 0-value(s)")
     }else{
         print("data doesn't include 0-value(s)")
     }
 
-    if(any(data < 0)){
+    if(any(assay(SE) < 0)){
         print("data includes negative value(s)")
     }else{
         print("data doesn't include negative value(s)")
@@ -41,24 +43,25 @@ FixZeros <- function(data, group, remove.sparse.features=TRUE) {
     if(remove.sparse.features==TRUE){
         smallestGr <- min(as.numeric(table(group))) # a size of the smallest
         #group of samples
-        greaterthanBG <- apply(data, 1, function(x) sum(x > 0)) #counting
+        greaterthanBG <- apply(assay(SE), 1, function(x) sum(x > 0)) #counting
         #non-zero counts for each feature
         lessthanBG  <- which(as.numeric(greaterthanBG) < smallestGr) #features
         #having number of zero counts higher than the size of the smallest
         #sample group.
 
         if (length(lessthanBG) > 0) {
-            data <- data[-lessthanBG,] #removal of lines with low counts
+            # data <- data[-lessthanBG,] #removal of lines with low counts
+            SE<-SE[-lessthanBG,]
             print(paste0(length(lessthanBG)," features will be removed because of the low counts"))
         }
     }
 
     ###Replacement of zeros by the lowest value for each feature
-    min_per_row <- as.vector(apply(data, 1, function(x) min(x[x != 0])))
-    for(i in 1:nrow(data)){
-        data[i, data[i,] == 0] <- min_per_row[i] #substitution of zeros with
+    min_per_row <- as.vector(apply(assay(SE), 1, function(x) min(x[x != 0])))
+    for(i in 1:nrow(assay(SE))){
+        assay(SE)[i, assay(SE)[i,] == 0] <- min_per_row[i] #substitution of zeros with
         #min values per row
     }
 
-    return(data)
+    return(SE)
 }
